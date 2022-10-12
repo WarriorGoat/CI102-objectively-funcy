eb089c04d09d22394537479eb5a0c17e463b2523
WarriorGoat <michael.d.spitz@gmail.com>
2022-10-12 14:37:16 -0400

    Minor updates.

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ function giveBirthday (person){
 function marry(person1, person2){
   person1.married = true;
   person2.married = true;
-  person1.spouseName = (`${person2.firstName} ${person2.lastName}`);
-  person2.spouseName = (`${person1.firstName} ${person1.lastName}`);
+  person1.spouseName = `${person2.firstName} ${person2.lastName}`;
+  person2.spouseName = `${person1.firstName} ${person1.lastName}`;
 }
 
 function divorce (person1, person2){
